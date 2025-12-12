2b359a0f6132c0c454880521dcdea1903e83c9d8
Kateryna <katyaukr2014@gmail.com>
2025-12-12 13:20:15 +0200

    add

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// Ініціалізація іконок
+document.addEventListener('DOMContentLoaded', () => {
+    lucide.createIcons();
+
+    // Тут будемо додавати логіку мобільного меню та анімацій
+    console.log('Myth-Tica scripts loaded. Технология нового поколения активирована.');
+});
